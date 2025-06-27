ce9fbdc60cb7491ddbd61e6c0745d6bc2709a3ec
sleepyyapril <flyingkarii@gmail.com>
2025-06-27 19:02:20 -0300

    Capital ships can no longer be crewed by a single person.

---
+++ Content.Shared/_Mono/Ships/Components/CrewedShuttleComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._Mono.Ships.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class CrewedShuttleComponent : Component
+{
+    
+}
