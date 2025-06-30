c034ab7be7602c74774768983c0f3ed0fa13e6fa
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-06-30 03:48:23 +0000

    chore: automatically update REUSE headers

--- Content.Server/_NF/Shipyard/Systems/ShipyardSystem.Consoles.cs
+++ Content.Server/_NF/Shipyard/Systems/ShipyardSystem.Consoles.cs
@@ -17,8 +17,8 @@
 // SPDX-FileCopyrightText: 2025 LukeZurg22
 // SPDX-FileCopyrightText: 2025 Redrover1760
 // SPDX-FileCopyrightText: 2025 Whatstone
-// SPDX-FileCopyrightText: 2025 sleepyyapril
 // SPDX-FileCopyrightText: 2025 ark1368
+// SPDX-FileCopyrightText: 2025 sleepyyapril
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
